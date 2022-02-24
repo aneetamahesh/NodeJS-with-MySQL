ca716a2153d81b08086f5b4ebf13c09af70f8e5f
aneetamahesh <96040024+aneetamahesh@users.noreply.github.com>
2022-02-24 19:40:37 +0530

    Delete clientscript.js

--- clientscript.js
+++
@@ -1,4 +0,0 @@
-var myapp=angular.module("myapp",[]);
-myapp.controller("myctrl",function($scope,$http){
-    $http.get("http://localhost:5500/display").success(function(response){$scope.names=response;})
-})
